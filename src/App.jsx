6044ed24557e2368c8a19b8543bb072382f292d6
Markost1 <markostevovic565@gmail.com>
2025-04-24 11:26:55 +0200

    Kreiranje stranica i ruta

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,29 @@
+import{BrowserRouter, Route, Routes} from 'react-router-dom'
+import Home from './pages/Home';
+import Sell from './pages/Sell';
+import Rent from './pages/Rent';
+import Blog from './pages/Blog';
+import Team from './pages/Team';
+import Contact from './pages/Contact';
+
+
 function App() {
+  
   return (
-    <div className="text-3xl font-bold text-blue-500">
-      Hello Tailwind!
-    </div>
+    <BrowserRouter>
+      
+      <Routes>
+
+        <Route path='/' element={<Home />} />
+        <Route path='/sell' element={<Sell />} />
+        <Route path='/rent' element={<Rent />} />
+        <Route path='/blog' element={<Blog />} />
+        <Route path='/team' element={<Team />} />
+        <Route path='/contact' element={<Contact />} />
+
+      </Routes>
+
+    </BrowserRouter>
   )
 }
 
