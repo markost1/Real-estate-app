06192218264dff586dfe5bd12105f4e844f17453
Markost1 <markostevovic565@gmail.com>
2025-04-25 08:23:50 +0200

    Konektovanje na MongoDB preko mongoose frameworka, sakrivanje stringa za konekciju dotenv paket.

--- api/index.js
+++ api/index.js
@@ -1,8 +1,19 @@
-import express from 'express'
+import express from 'express';
+import mongoose from 'mongoose';
+import 'dotenv/config';
 
 const app = express()
+
+mongoose.connect(process.env.MONGO).then(()=>{
+    console.log('Connected to MongoDB');
+    
+}).catch((err)=>{
+    console.log(err);
+    
+})
 
 app.listen(3000,()=>{
     console.log('server je aktivan na portu 3000!');
     
 })
+
